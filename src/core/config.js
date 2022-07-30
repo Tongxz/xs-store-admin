3ba61bd159cb72db9402e28b3a1fa29d924fb705
tongxiaozhou <bluestonea@foxmail.com>
2022-07-30 21:35:52 +0800

    fix: 系统名称修改

--- src/core/config.js
+++ src/core/config.js
@@ -3,7 +3,7 @@
  */
 
 const config = {
-  appName: '香石管理系统',
+  appName: '香石门店管理系统',
   appLogo: 'https://xs-qingshi.oss-cn-shanghai.aliyuncs.com/oss%3A//xs-qingshi//uploads/2022-06-30/IMG_4313.JPG',
   showViteLogo: true
 }
